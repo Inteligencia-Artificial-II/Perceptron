83fcf4456e87eb64ff82ca2fcb03032a32c44661
angelhumberto99 <aangel.gutierrez@alumnos.udg.mx>
2022-01-30 15:18:00 -0600

    inicio del algoritmo de evaluación

--- src/Perceptron.py
+++ src/Perceptron.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 from src.UI import render_ui
+import numpy as np
 
 class Perceptron:
     def __init__(self):
@@ -10,6 +11,15 @@ class Perceptron:
         self.ax.set_ylim([-5, 5])
         # guarda la información de los puntos sobre la gráfica
         self.data = []
+        # puntos para evaluar
+        self.test_data = []
+        # bandera para evaluar datos despues del entrenamiento
+        self.is_training = True
+
+        self.gens = 100 # epocas o generaciones máximas
+        self.W = [] # pesos
+        self.lr = 0.1 # tasa de aprendizaje
+
         # llama a la interfaz gráfica
         render_ui(self)
 
@@ -22,15 +32,74 @@ class Perceptron:
         if (event.xdata == None or event.ydata == None): return
 
         # guardamos una tupla con los valore X y Y, así como su clase correspondiente
-        self.data.append(((event.xdata, event.ydata), cluster[1]))
+        if (self.is_training): 
+            self.data.append(((event.xdata, event.ydata), cluster[1]))
+            plt.plot(event.xdata, event.ydata, cluster[0], color=color)
+        else:
+            self.test_data.append((event.xdata, event.ydata))
+            plt.plot(event.xdata, event.ydata, 'o', color='k')
 
-        plt.plot(event.xdata, event.ydata, cluster[0], color=color)
         self.fig.canvas.draw()
 
     def run(self):
-        print("Iteraciones máximas: ", self.max_iter.get())
-        print("Tasa de aprendizaje: ", self.learning_rate.get())
-        print("Data: ", self.data)
+        self.is_training = False
+        self.container_before.grid_remove()
+        self.container_after.grid(row=2, columnspan=4)
+        self.lr = self.learning_rate.get()
+        self.gens = self.max_iter.get()
+        self.train()
+
+        # llamar al algoritmo de entrenamiento
 
     def init_weights(self):
         print("Inicializando pesos")
+    
+    def evaluate(self):
+        print("Test data: ", self.test_data)
+        # llamar al algoritmo de entrenamiento
+
+
+    def train(self):
+        # obtenemos los valores de X
+        aux_arr = [[self.data[i][0][0], self.data[i][0][1]] for i in range(len(self.data))]
+        X = np.array(aux_arr[:])
+        del aux_arr
+        # obtenemos los valores de Y (las clases)
+        aux_arr = [self.data[i][1] for i in range(len(self.data))]
+        Y = np.array(aux_arr[:])
+
+        m, n = X.shape
+        y = np.zeros((m))
+        v = np.empty((m))
+        conv = []
+
+        # Normalización
+        mu = np.mean(X, axis=0)
+        sigma = np.std(X, axis=0)
+        xNorm = []
+        for i in range(n):
+            xNorm.append((X[:,i] - mu[i])/sigma[i])
+        X = np.transpose(np.array(xNorm))
+        b = -0.5
+        W = [0.2, 0.5]
+        a = 0.03
+        it = 0
+        error = np.sum((np.abs(Y-y)))
+
+
+        while(error != 0):
+            for i in range(m):
+                v[i] = np.dot(W, X[i]) + b
+                if v[i] > 0:
+                    y[i] = 1
+                else:
+                    y[i] = 0
+                if (Y[i] - y[i]) != 0:
+                    W = W + a*X[i]
+                    b = b + a
+                error = np.sum(np.abs(Y-y))
+                conv.append(error)
+                it += 1
+        
+        print("si me salio jefa: ", W)
+
--- src/UI.py
+++ src/UI.py
@@ -12,21 +12,24 @@ def render_ui(self):
     # añade el gráfico de matplotlib a la interfaz de tkinter
     FigureCanvasTkAgg(self.fig, self.window).get_tk_widget().grid(row=1, columnspan=4)
 
-    # contendrá un segmento de la interfaz
-    self.container = Frame(self.window)
+    # contendrá un segmento de la interfaz (valores para entrenar)
+    self.container_before = Frame(self.window)
 
-    Label(self.container, text="Epocas máximas:").grid(row=0, column=0)
-    self.max_iter = Entry(self.container)
+    Label(self.container_before, text="Epocas máximas:").grid(row=0, column=0)
+    self.max_iter = Entry(self.container_before)
     self.max_iter.grid(row=0, column=1)
 
-    Label(self.container, text="Tasa de aprendizaje:").grid(row=1, column=0)
-    self.learning_rate = Entry(self.container)
+    Label(self.container_before, text="Tasa de aprendizaje:").grid(row=1, column=0)
+    self.learning_rate = Entry(self.container_before)
     self.learning_rate.grid(row=1, column=1)
 
-    Button(self.container, text="Inicializar pesos", command=self.init_weights).grid(row=0, column=2)
-    Button(self.container, text="Analizar", command=self.run).grid(row=1, column=2)
+    Button(self.container_before, text="Inicializar pesos", command=self.init_weights).grid(row=0, column=2)
+    Button(self.container_before, text="Entrenar", command=self.run).grid(row=1, column=2)
 
-    self.container.grid(row=2, columnspan=4)
+    self.container_before.grid(row=2, columnspan=4)
+
+    self.container_after = Frame(self.window)
+    self.analyse = Button(self.container_after, text="Evaluar", command=self.evaluate).grid(row=0, column=0)
 
     # escucha los eventos del mouse sobre el gráfico
     self.fig.canvas.mpl_connect('button_press_event', self.set_point)
